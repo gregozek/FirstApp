45ea52f9b641de0fb7f8794c8fa50cdab6a56196
David <david@arqetype.com>
2014-07-29 12:39:22 -0700

    Initializing repo -dmg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '35abe7e39f7fc7d322032e0e3179c8ebbdb82704ac864f9614c198bf8713d007a8375a6e419e9967a4bf2a870689add39e3132064a9c85206d32eea9856d1384'
